71aba434b082682ca6274cfcad6812ba0694c0aa
Yoav Arad <yoavarad10@gmail.com>
2021-04-23 16:59:44 +0300

    simulate_hits + simulate_throw apis

--- bowling_api.py
+++ bowling_api.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def create_video(X_values, Y_values):
+def create_video(all_obj_locs):
     plt.scatter()
 
 
@@ -18,8 +18,34 @@ def memoize(f):
 
 
 @memoize
-def simulate_hits(x, y, vx, vy, wx, wy):
-    pass
+def simulate_throw(x, vx, vy, wx, wy, show_video=False):
+    ball_locs = []
+    ball_stats = np.array([x, 0, vx, vy, wx, wy])
+    while not reached_pins(ball_stats):
+        calc_throw_dt(ball_stats)  # TODO MED
+        ball_locs.append(ball_stats[:2])
+
+    if show_video:
+        create_video(ball_locs)
+    return ball_stats
+
+
+def init_pins():
+    return np.zeros((10, 12))
+
+
+@memoize
+def simulate_hits(x, y, vx, vy, wx, wy, show_video=False):
+    all_obj_locs = []
+    ball_stats = np.array([x, y, vx, vy, wx, wy])
+    pins_stats = init_pins()
+    while not throw_ended(ball_stats, pins_stats):
+        calc_hits_dt(ball_stats, pins_stats)  # TODO HARD
+        all_obj_locs.append(get_locs(pins_stats, ball_stats))
+
+    if show_video:
+        create_video(all_obj_locs)
+    return calc_score(pins_stats)
 
 
 def main():
@@ -27,8 +53,8 @@ def main():
     scores = np.zeros((error_rates, throw_num_per_error_rate))
     for i, error_rate in enumerate(error_rates):
         for j in range(throw_num_per_error_rate):
-            x, vx, vy, wx, wy = get_throwing_parameters(error_rate)  # y default is 0
-            x, y, vx, vy, wx, wy = calc_first_hit_position(x, 0, vx, vy, wx, wy)
+            x, vx, vy, wx, wy = get_random_throwing_parameters(error_rate)  # y default is 0
+            x, y, vx, vy, wx, wy = simulate_throw(x, 0, vx, vy, wx, wy)
             score = simulate_hits(x, y, vx, vy, wx, wy)
             scores[i, j] = score
     avg_hits = np.average(scores, axis=1)
@@ -36,4 +62,3 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
