71deaa13bd7089d19fc1212bf8b5d8b32b109777
matanepel <matan.epel12@gmail.com>
2021-04-27 14:11:22 +0300

    only hard work left

--- bowling_api.py
+++ bowling_api.py
@@ -1,7 +1,22 @@
 from math import dist
-
 import matplotlib.pyplot as plt
 import numpy as np
+
+"""
+BALL: (x, y, vx, vy, wx, wy)
+pin: (x1, y1, z1, x2, y2, z2, vx, vy, vz, wx, wy, wz
+"""
+
+# TODO:
+# 1. finding mu of oiled lane
+# 2. finding the best throwing parameters
+# 3. deciding on max variance we wnt in our throws (error rate will be in percentage out of this variance)
+# 4. joining frames to video
+# 5. finding the correct locations of the pins
+# 6. creating the init_pins function (agter 5)
+# 7. correcting the throw_dt function
+# 8. writing the hit_dt function
+
 
 R_BALL = 10.795
 R_PIN = 5
@@ -17,21 +32,20 @@ BOWLING_SIZE = np.pi * R_BALL ** 2
 PIN_HEIGHT = 40
 ERR_RT = 0.01
 REPEATITIONS = 100
-
-# TODO: change values
+# TODO: change values:
 BEST_VY = 8
 BEST_VX = 4
 BEST_WX = 5
 BEST_WY = 3
 DEFAULT_VAR = 0.1
 
-"""
-BALL: (x, y, vx, vy, wx, wy)
-pin: (x1, y1, z1, x2, y2, z2, vx, vy, vz, wx, wy, wz
-"""
-
 
 def create_video(all_obj_locs):
+    """
+    creating all the frames for the video by the locations of the ball and pins
+    :param all_obj_locs: locations (x,y) of the ball and the pins
+    :return: none
+    """
     i = 0
     for f in all_obj_locs:
         plt.figure(figsize=(SIZE * 2, SIZE), dpi=80)
@@ -47,6 +61,11 @@ def create_video(all_obj_locs):
 
 
 def memoize(f):
+    """
+    memoizing function we use twce to save running time
+    :param f: the function
+    :return: the saving function
+    """
     memo = {}
 
     def helper(*x):
@@ -58,20 +77,32 @@ def memoize(f):
 
 
 def still_going(ball_stats):
-    if ball_stats[3] <= 0 and ball_stats[2] <= 0:
+    """
+    checking if the simulation of the ball throwing should continue
+    we stop in case were the ball does not move or he hitted one of the pins
+    :param ball_stats: velocity and location of the ball
+    :return: if the ball should continue or not
+    """
+    if ball_stats[3] == 0 and ball_stats[2] == 0:  # if vy = vx = 0 we should stop
         return False
     pins_loc = [(720, 20.5), (730.375, 26.5), (740.75, 32.5), (751.125, 38.5)
-        , (730.375, 14.5), (740.75, 8.5), (751.125, 2.5)]
-    pins_loc = [(2.54 * p[0], 2.54 * p[1]) for p in pins_loc]
-    if ball_stats[0] > 41 * 2.54 or ball_stats[0] < 0:
+        , (730.375, 14.5), (740.75, 8.5),
+                (751.125, 2.5)]  # the locations of the pins in inch's. TODO: update to correct
+    pins_loc = [(2.54 * p[0], 2.54 * p[1]) for p in pins_loc]  # converting to cm
+    if ball_stats[0] > 41 * 2.54 or ball_stats[0] < 0:  # checking if we are out of the lane
         return False
     for p in pins_loc:
-        if dist((ball_stats[0], ball_stats[1]), p) < R_BALL + R_PIN:
+        if dist((ball_stats[0], ball_stats[1]), p) < R_BALL + R_PIN:  # checking if we hit one of the balls
             return False
     return True
 
 
 def calc_throw_dt(ball_stats):
+    """
+    calculating the next location of the ball
+    :param ball_stats: the ball stats (x,y,vx,vy,wx,wy)
+    :return: the new ball stats
+    """
     # ball stats: [0] x=x1, [1] y=x2, [2] vx=x3, [3] vy=x4, [4] wx=x5, [5] wy=x6
     Fx1, Fx2 = -OILED_MU * BALL_M * G * np.sign(ball_stats[2:4])
     ball_stats = ball_stats + np.array(
@@ -79,6 +110,42 @@ def calc_throw_dt(ball_stats):
         [ball_stats[2], ball_stats[3], Fx1 / BALL_M, Fx2 / BALL_M, Fx2 * R_BALL / BALL_I, -Fx1 * R_BALL / BALL_I]
     )
     return ball_stats
+
+
+def get_locs(pins_stats, ball_stats):
+    """
+    getting all the locations of the ball and pins
+    :param pins_stats: the pins stats
+    :param ball_stats: the ball stats
+    :return: the ball and pins locations
+    """
+    return pins_stats[:, :6], ball_stats[:2]
+
+
+def calc_score(pins_stats):
+    """
+    calculating the score we have after the throw
+    :param pins_stats:
+    :return:
+    """
+    count = 0
+    for p in pins_stats:
+        if p[5] < PIN_HEIGHT / 2:  # p[5] = z of top pin position
+            count += 1  # if z < 0.5 of the top position then the pin fell
+    return count
+
+
+def throw_ended(ball_stats, pins_stats):
+    """
+    checking if we need to stop (ball out of lane/stopped or all the pins stopped)
+    :param ball_stats:
+    :param pins_stats:
+    :return:
+    """
+    ball_stopped = (ball_stats[2] == 0 and ball_stats[3] == 0) or ball_stats[0] < 0 or ball_stats[0] > LANE_WIDTH or \
+                   ball_stats[1] > LANE_LENGTH
+    pins_stopped = all(p[6] == 0 and p[7] == 0 and p[8] == 0 for p in pins_stats)  # p[6:8] = vx, vy, vz
+    return ball_stopped and pins_stopped
 
 
 @memoize
@@ -100,24 +167,6 @@ def init_pins():
     return np.zeros((10, 12))  # TODO
 
 
-def get_locs(pins_stats, ball_stats):
-    return pins_stats[:, :6], ball_stats[:2]
-
-
-def calc_score(pins_stats):
-    count = 0
-    for p in pins_stats:
-        if p[5] < PIN_HEIGHT / 2:  # p[5] = z of top pin position
-            count += 1
-    return count
-
-
-def throw_ended(ball_stats, pins_stats):
-    ball_stopped = ball_stats[2] == 0 and ball_stats[3] == 0
-    pins_stopped = all(p[6] == 0 and p[7] == 0 and p[8] == 0 for p in pins_stats)  # p[6:8] = vx, vy, vz
-    return ball_stopped and pins_stopped
-
-
 @memoize
 def simulate_hits(x, y, vx, vy, wx, wy, show_video=False):
     all_obj_locs = []
@@ -133,6 +182,12 @@ def simulate_hits(x, y, vx, vy, wx, wy, show_video=False):
 
 
 def plot_graph(error_rates, avg_hits):
+    """
+    plotting the graph of average hits per error rate
+    :param error_rates: the error rate list
+    :param avg_hits: the hits list per error rate
+    :return: none
+    """
     plt.xlabel("Error rates (σ)")
     plt.ylabel("Average pins hit")
     plt.plot(error_rates, avg_hits)
@@ -140,10 +195,19 @@ def plot_graph(error_rates, avg_hits):
 
 
 def get_error_rates():
+    """
+    getting a list of error rates to check
+    :return: the list
+    """
     return [(ERR_RT) * i for i in range(int((1 / ERR_RT) / 4))]  # error up to 25%
 
 
 def get_random_throwing_parameters(error_rate):
+    """
+    getting the trowing parameters by error rate (based on the best throw)
+    :param error_rate: the error rate
+    :return: the throwing parameters
+    """
     return np.random.normal(LANE_WIDTH / 2, error_rate * DEFAULT_VAR), np.random.normal(BEST_VX,
                                                                                         error_rate * DEFAULT_VAR), \
            np.random.normal(BEST_VY, error_rate * DEFAULT_VAR), np.random.normal(BEST_WX, error_rate * DEFAULT_VAR), \
@@ -165,5 +229,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
-    print(get_random_throwing_parameters(0.1))
+    main()
